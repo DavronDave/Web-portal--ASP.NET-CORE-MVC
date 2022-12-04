c00ce81e14a66b28935d5ee0261d51ddae0ce894
DavronDave <davronomonov99@gmail.com>
2022-12-04 12:35:18 +0500

    Add project files.

---
+++ DataLayer/Data/UserDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Data
+{
+    public class UserDbContext : IdentityDbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
